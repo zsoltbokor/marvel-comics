0baf13cde6f5e92fc08eb589bec6e9c567d0edd8
Zsolt Bokor <bokor.zsolt@sonrisa.hu>
2021-08-24 12:09:18 +0300

    test

--- pages/comics/[id]/index.tsx
+++ pages/comics/[id]/index.tsx
@@ -4,13 +4,46 @@ import {getURL} from "../../../utils/fnUtils";
 
 const ComicDetailPage: FC<{ comic }> = ({comic}) => {
 
+    if (!comic) {
+        return null;
+    }
+
     return (
         <PageComicDetails details={comic}/>
     )
 }
 
 // @ts-ignore
-ComicDetailPage.getInitialProps = async (context) => {
+// ComicDetailPage.getInitialProps = async (context) => {
+//
+//     const result = await fetch(getURL(`comics/${context.query.id}`), {
+//         method: 'GET'
+//     });
+//
+//     const comics = await result.json();
+//
+//     return {
+//         comic: comics.results[0]
+//     }
+// }
+
+export default ComicDetailPage;
+
+export const getStaticPaths = async () => {
+    const result = await fetch(getURL(`comics?orderBy=-onsaleDate`), {
+        method: 'GET'
+    });
+    const comics = await result.json();
+
+    return {
+        paths: comics.results.map(comic => {
+            return {params: {id: `${comic.id}`}};
+        }),
+        fallback: true
+    }
+}
+//
+export const getStaticProps = async (context) => {
     const result = await fetch(getURL(`comics/${context.params.id}`), {
         method: 'GET'
     });
@@ -18,37 +51,9 @@ ComicDetailPage.getInitialProps = async (context) => {
     const comics = await result.json();
 
     return {
-        comic: comics.results[0]
+        props: {
+            comic: comics.results[0]
+        },
+        revalidate: 60
     }
 }
-
-export default ComicDetailPage;
-
-// export const getStaticPaths = async () => {
-//     const result = await fetch(getURL(`comics?orderBy=-onsaleDate`), {
-//         method: 'GET'
-//     });
-//     const comics = await result.json();
-//
-//     return {
-//         paths: comics.results.map(comic => {
-//             return {params: {id: `${comic.id}`}};
-//         }),
-//         fallback: true
-//     }
-// }
-//
-// export const getStaticProps = async (context) => {
-//     const result = await fetch(getURL(`comics/${context.params.id}`), {
-//         method: 'GET'
-//     });
-//
-//     const comics = await result.json();
-//
-//     return {
-//         props: {
-//             comic: comics.results[0]
-//         },
-//         revalidate: 60
-//     }
-// }
