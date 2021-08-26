12544acdd219b43611b0bb189e3f75a38496201e
Zsolt Bokor <bokor.zsolt@sonrisa.hu>
2021-08-26 14:15:54 +0300

    character + series detail page

--- pages/series/[id]/index.tsx
+++ pages/series/[id]/index.tsx
@@ -3,45 +3,50 @@ import {getURL} from "../../../utils/fnUtils";
 import {PageSeriesDetails} from "../../../components/Page/PageSeriesDetails";
 import Head from "next/head";
 
-const SeriesDetailPage: FC<{series}> = ({series}) => {
-  return (
-      <>
-        <Head>
-          <title>{`${series.title} - Comics - Marvel Universe`}</title>
-          <meta name="description" content={series.description} />
-        </Head>
-        <PageSeriesDetails data={series} />
-      </>
-  )
+const SeriesDetailPage: FC<{ series }> = ({series}) => {
+
+    if (!series) {
+        return null;
+    }
+
+    return (
+        <>
+            <Head>
+                <title>{`${series.title} - Comics - Marvel Universe`}</title>
+                <meta name="description" content={series.description}/>
+            </Head>
+            <PageSeriesDetails data={series}/>
+        </>
+    )
 }
 
 export default SeriesDetailPage;
 
 export const getStaticPaths = async () => {
-  const result = await fetch(getURL(`series`), {
-    method: 'GET'
-  });
-  const series = await result.json();
+    const result = await fetch(getURL(`series`), {
+        method: 'GET'
+    });
+    const series = await result.json();
 
-  return {
-    paths: series.results.map(s => {
-      return {params: {id: `${s.id}`}};
-    }),
-    fallback: true
-  }
+    return {
+        paths: series.results.map(s => {
+            return {params: {id: `${s.id}`}};
+        }),
+        fallback: true
+    }
 }
 
 export const getStaticProps = async (context) => {
-  const result = await fetch(getURL(`series/${context.params.id}`), {
-    method: 'GET'
-  });
+    const result = await fetch(getURL(`series/${context.params.id}`), {
+        method: 'GET'
+    });
 
-  const series = await result.json();
+    const series = await result.json();
 
-  return {
-    props: {
-      series: series.results[0]
-    },
-    revalidate: 60
-  }
+    return {
+        props: {
+            series: series.results[0]
+        },
+        revalidate: 60
+    }
 }
