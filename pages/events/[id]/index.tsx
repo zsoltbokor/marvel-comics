0bbf3b5bfd3c7ebd8110bfa77d89d4ddc73f03c0
zs01ltika <zsoly91@gmail.com>
2021-08-27 11:01:48 +0300

    enable pages

--- pages/events/[id]/index.tsx
+++ pages/events/[id]/index.tsx
@@ -1,6 +1,7 @@
 import {FC} from "react";
 import {PageEventDetails} from "../../../components/Page/PageEventDetails";
 import Head from "next/head";
+import {getURL} from "../../../utils/fnUtils";
 
 const EventDetailPage: FC<{ event }> = ({event}) => {
 
@@ -22,31 +23,31 @@ const EventDetailPage: FC<{ event }> = ({event}) => {
 
 export default EventDetailPage;
 
-// export const getStaticPaths = async () => {
-//   const result = await fetch(getURL(`events`), {
-//     method: 'GET'
-//   });
-//   const events = await result.json();
-//
-//   return {
-//     paths: events.results.map(s => {
-//       return {params: {id: `${s.id}`}};
-//     }),
-//     fallback: true
-//   }
-// }
-//
-// export const getStaticProps = async (context) => {
-//   const result = await fetch(getURL(`events/${context.params.id}`), {
-//     method: 'GET'
-//   });
-//
-//   const events = await result.json();
-//
-//   return {
-//     props: {
-//       event: events.results[0]
-//     },
-//     revalidate: 60
-//   }
-// }
+export const getStaticPaths = async () => {
+  const result = await fetch(getURL(`events`), {
+    method: 'GET'
+  });
+  const events = await result.json();
+
+  return {
+    paths: events.results.map(s => {
+      return {params: {id: `${s.id}`}};
+    }),
+    fallback: true
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const result = await fetch(getURL(`events/${context.params.id}`), {
+    method: 'GET'
+  });
+
+  const events = await result.json();
+
+  return {
+    props: {
+      event: events.results[0]
+    },
+    revalidate: 60
+  }
+}
