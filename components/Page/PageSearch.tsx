7707ec53ea29d691c82f7521c78797730cfcbb75
zs01ltika <zsoly91@gmail.com>
2021-08-30 14:39:41 +0300

    transform search

--- components/Page/PageSearch.tsx
+++ components/Page/PageSearch.tsx
@@ -1,17 +1,17 @@
 import {InputWrapper, NoResult, ResultWrapper, SearchButton, SearchInput, Wrapper} from "./PageSearch.css";
 import Icon from '../../public/search.svg';
-import {useEffect, useRef, useState} from "react";
+import {FC, useEffect, useRef, useState} from "react";
 import {useRouter} from "next/router";
-import {getURL} from "../../utils/fnUtils";
 import {LoadingIcon} from "../../style/reusable";
 import {Grid} from "../Grid/Grid";
 
-export const PageSearch = () => {
+import Router from 'next/router';
+
+export const PageSearch: FC<{ result }> = ({result}) => {
 
     const inputRef = useRef<HTMLInputElement>();
     const router = useRouter();
     const [searching, setSearching] = useState<boolean>(false);
-    const [result, setResult] = useState<any | null>(null);
 
     const doSearch = () => {
         router.replace(`/search?q=${inputRef.current.value}`);
@@ -27,26 +27,19 @@ export const PageSearch = () => {
         if (!router.query.q) {
             return;
         }
-
         inputRef.current.value = router.query.q as string;
-
         setSearching(true);
-        const response = await fetch(getURL(`search?q=${router.query.q}`));
-        const data = await response.json();
-
-        setResult(data);
-        setSearching(false);
     }
 
     const hasResult = () => {
-        if(!result) {
+        if (!result) {
             return false;
         }
 
         let hasResult = false;
 
         Object.keys(result).forEach(group => {
-            if(result[group]?.data?.length) {
+            if (result[group]?.data?.length) {
                 hasResult = true;
             }
         })
@@ -54,12 +47,31 @@ export const PageSearch = () => {
         return hasResult;
     };
 
+    const searchStarted = () => setSearching(true);
+    const searchEnded = () => setSearching(false);
+
     useEffect(() => {
         if (router.query) {
             makeSearch();
         }
 
     }, [router.query]);
+
+    useEffect(() => {
+        setSearching(false);
+    }, [result])
+
+    useEffect(()=>{
+        Router.events.on('routeChangeStart', searchStarted)
+        Router.events.on('routeChangeComplete', searchEnded)
+        Router.events.on('routeChangeError', searchEnded)
+
+        return () => {
+            Router.events.off('routeChangeStart', searchStarted)
+            Router.events.off('routeChangeComplete', searchEnded)
+            Router.events.off('routeChangeError', searchEnded)
+        }
+    }, [])
 
     return (
         <Wrapper>
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,16 +1,29 @@
 import {FC} from "react";
 import {PageSearch} from "../../components/Page/PageSearch";
 import Head from "next/head";
+import {getURL} from "../../utils/fnUtils";
 
-const SearchPage: FC = () => {
+const SearchPage: FC<{result}> = ({result}) => {
     return (
         <>
             <Head>
                 <title>Search - Marvel Universe</title>
             </Head>
-            <PageSearch />
+            <PageSearch result={result} />
         </>
     );
 }
 
 export default SearchPage;
+
+
+export const getServerSideProps = async (context) => {
+    const response = await fetch(getURL(`search?q=${context.query.q}`));
+    const data = await response.json();
+
+    return {
+        props: {
+            result: data
+        }
+    }
+}
