39de39c7bb0e31482b5b2781a3b0c61bed6f339c
zs01ltika <zsoly91@gmail.com>
2021-08-30 11:14:47 +0300

    fix router error

--- hooks/useLocation.ts
+++ hooks/useLocation.ts
@@ -4,18 +4,14 @@ import { useMemo } from 'react';
 export const useLocation = () => {
     const router = useRouter();
 
-    if (!router) {
-        throw new Error('NextRouterContext not defined');
-    }
-
     return useMemo(
         () => ({
-            pathname: router.asPath.split('?')[0],
-            query: router.query,
-            search: router.asPath?.split(/\?/)?.[1]?.split(/#/)?.[0],
-            hash: router.asPath?.split(/#/)?.[1],
+            pathname: router?.asPath.split('?')[0],
+            query: router?.query,
+            search: router?.asPath?.split(/\?/)?.[1]?.split(/#/)?.[0],
+            hash: router?.asPath?.split(/#/)?.[1],
             state: null,
         }),
-        [router.asPath]
+        [router?.asPath]
     );
 };
